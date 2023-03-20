3eee2c52ab4dd82c8844b1bf66f45b84718b7ac6
bete7512 <betekbebe@gmail.com>
2023-03-20 10:54:21 +0300

    do hard thing

---
+++ backend/express/pm2.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps:[
+        {
+            name:'server',
+            script:'server.js',
+            env:{
+                MODE_ENV: 'production'
+            },
+            exec_mode: 'cluster',
+            instances: 'max'
+        }
+    ]
+}
